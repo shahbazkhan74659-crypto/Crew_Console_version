007e892ac23ad6ab3aa0c81bbe4329b7d0468a4c
Shahbaz <shahbazkhan74659@gmail.com>
2026-06-21 14:24:39 +0530

    Added StaticFiles & Final Development

--- Crew/settings.py
+++ Crew/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7f(!n3*%b4dyc#$dwl*&*p5q3g!mpad0i8i_qd%p=r6ey0bpb4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
